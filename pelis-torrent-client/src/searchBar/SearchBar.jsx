6d708c2357cdef7d3ada85e57f9a1ecf921c4d07
Federico Jure <fjure@atixlabs.com>
2020-06-08 23:26:32 -0300

    agrego estilo a barra de busqueda

--- pelis-torrent-client/src/searchBar/SearchBar.jsx
+++ pelis-torrent-client/src/searchBar/SearchBar.jsx
@@ -7,7 +7,10 @@ const SearchBar = ({onChange}) => {
         <SelectSearch
             getOptions={(query) => {
                 return new Promise((resolve, reject) => {
-                    fetch(`https://api.themoviedb.org/3/search/movie?api_key=133f4d8b4fed128b27fa0bb407956c56&language=en-US&page=1&include_adult=false&query=${query}`)
+                    fetch(`https://api.themoviedb.org/3/search/movie?`+
+                            `api_key=133f4d8b4fed128b27fa0bb407956c56&`+
+                            `language=en-US&page=1&include_adult=false&`+
+                            `query=${query}`)
                         .then(response => response.json())
                         .then(({results}) => {
                             resolve(results.map(({ title }) => ({ value: title, name: title })))
@@ -18,6 +21,20 @@ const SearchBar = ({onChange}) => {
             search
             placeholder="Torrent to search"
             onChange={onChange}
+            renderOption={props =>
+            <div className="search-item">
+                <div>
+                    <img src="./logo192.png"/>
+                </div>
+                <div>
+                    <a>{props.value}</a>
+                    <div className="search-item-description">
+                        <span>Author</span>
+                        <span>2020</span>
+                        <span>5</span>
+                    </div>
+                </div>
+            </div>}
         />
     );
 }
