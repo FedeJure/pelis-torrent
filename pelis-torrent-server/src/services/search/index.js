7ec1dc1baffef42ca66f87610082b266bbe91334
federico.jure <federico.jure@etermax.com>
2020-06-10 17:42:24 -0300

    WIP - devolver varios torrents

--- pelis-torrent-server/src/services/search/index.js
+++ pelis-torrent-server/src/services/search/index.js
@@ -3,13 +3,28 @@ const stringSimilarity = require('string-similarity');
 /**parameters allowed: name, category... */
 module.exports = async function (fastify, opts) {
   fastify.get('/search', async function (request, reply) {
-
     const torrents = await fastify.torrent.search(request.query.name, 'Movies', 20);
-    const torrentsName = torrents.map(t => t.title);
+    const sortedTorrents = torrents.sort((t1, t2) => compareTorrents(request.query.name,t1, t2))
+    /*const torrentsName = torrents.map(t => t.title);
     const bestMatch = stringSimilarity.findBestMatch(request.query.name, torrentsName)
-    const response = await fastify.torrent.getMagnet(torrents[bestMatch.bestMatchIndex]);
-    reply.status(200).send({response});
+    const response = await fastify.torrent.getMagnet(torrents[bestMatch.bestMatchIndex]);*/
+    reply.status(200).send({response: sortedTorrents.slice(0,10)});
   })
 }
 
-const compareSeeds = ( a, b ) => { return b.seeds - a.seeds; }
+const compareTorrents = (pivote, torrent1, torrent2 ) => {
+   return stringSimilarity.compareTwoStrings(pivote, torrent2.title)
+          - stringSimilarity.compareTwoStrings(pivote, torrent1.title); 
+}
+
+const containsKeywords = (keyWords, s2) => {
+  var containsAll = true;
+  for (let index = 0; index < keyWords.length; index++) {
+    const element = array[index];
+    if (!s2.includes(element)) {
+      containsAll = false;
+      break;
+    }
+  }
+  return containsAll;
+}
